3856065a8df14627fef45a2322cf6b59539f411c
Leta <31357777+Lita6@users.noreply.github.com>
2022-07-10 22:07:28 -0500

    Added tests, can add, subtract, multiply, divide and handle parentheses

--- code/win32_compiler.cpp
+++ code/win32_compiler.cpp
@@ -115,49 +115,160 @@ GetChar
 (u8 *ch, u8 *chMax)
 {
     
-    ch++;
-    if(ch >= chMax)
-    {
-        ch = 0;
+    if(ch != 0)
+    {    
+        ch++;
+        if(ch >= chMax)
+        {
+            ch = 0;
+        }
     }
+    
     return(ch);
 }
 
-void
-Term
-(Buffer *buffer, u8 *ch)
+u8 *Expression(Buffer *buffer, u8 *ch, u8 *chMax);
+
+u8 *
+Factor
+(Buffer *buffer, u8 *ch, u8 *chMax)
 {
-    u32 num = 0;
-    if(isDigit(*ch))
+    if(*ch == '(')
     {
-        num = (u32)((s32)(*ch) - '0');
+        ch = GetChar(ch, chMax);
+        ch = Expression(buffer, ch, chMax);
         
-        // mov eax, imm32
-        buffer_append_u8(buffer, 0xb8);
-        buffer_append_u32(buffer, num);
+        if(*ch != ')')
+        {
+            Assert(!"Closing parenthesis expected.");
+        }
     }
     else
     {
-        Assert(!"Only a single digit number accepted for now.");
+        u32 num = 0;
+        if(isDigit(*ch))
+        {
+            num = (u32)((s32)(*ch) - '0');
+            
+            // mov eax, imm32
+            buffer_append_u8(buffer, 0xb8);
+            buffer_append_u32(buffer, num);
+        }
+        else
+        {
+            Assert(!"Only a single digit number accepted for now.");
+        }
     }
+    
+    return(ch);
 }
 
-void
-Add
-(Buffer *buffer, u8 *ch)
+u8 *
+Multiply
+(Buffer *buffer, u8 *ch, u8 *chMax)
 {
-    Term(buffer, ch);
+    ch = Factor(buffer, ch, chMax);
+    
+    // pop ecx
+    buffer_append_u8(buffer, 0x59);
+    
+    // imul eax, ecx
+    buffer_append_u8(buffer, 0x0f);
+    buffer_append_u8(buffer, 0xaf);
+    buffer_append_u8(buffer, 0xc1);
+    
+    return(ch);
+}
+
+u8 *
+Divide
+(Buffer *buffer, u8 *ch, u8 *chMax)
+{
+    ch = Factor(buffer, ch, chMax);
+    
+    // xor edx, edx
+    buffer_append_u8(buffer, 0x31);
+    buffer_append_u8(buffer, 0xd2);
+    
+    // mov ecx, eax
+    buffer_append_u8(buffer, 0x89);
+    buffer_append_u8(buffer, 0xc1);
+    
+    // pop eax
+    buffer_append_u8(buffer, 0x58);
+    
+    // idiv ecx
+    buffer_append_u8(buffer, 0xf7);
+    buffer_append_u8(buffer, 0xf9);
+    
+    return(ch);
+}
+
+u8 *
+Term
+(Buffer *buffer, u8 *ch, u8 *chMax)
+{
+    ch = Factor(buffer, ch, chMax);
+    ch = GetChar(ch, chMax);
+    
+    while(ch != 0 && ((*ch == '*') || (*ch == '/')))
+    {    
+        // push rax
+        buffer_append_u8(buffer, 0xff);
+        buffer_append_u8(buffer, 0xf0);
+        
+        u8 c = *ch;
+        ch = GetChar(ch, chMax);
+        
+        switch(c)
+        {
+            case '*':
+            {
+                ch = Multiply(buffer, ch, chMax);
+            }break;
+            
+            case '/':
+            {
+                ch = Divide(buffer, ch, chMax);
+            }break;
+            
+            default:
+            {
+                Assert(!"Unsupported operation.");
+            };
+        }
+        
+        ch = GetChar(ch, chMax);
+        
+    }
+    
+    return(ch);
+}
+
+u8 *
+Add
+(Buffer *buffer, u8 *ch, u8 *chMax)
+{
+    ch = Term(buffer, ch, chMax);
+    
+    // pop ecx
+    buffer_append_u8(buffer, 0x59);
     
     // add eax, ecx
     buffer_append_u8(buffer, 0x01);
     buffer_append_u8(buffer, 0xc8);
+    
+    return(ch);
 }
 
-void
+u8 *
 Sub
-(Buffer *buffer, u8 *ch)
+(Buffer *buffer, u8 *ch, u8 *chMax)
 {
-    Term(buffer, ch);
+    ch = Term(buffer, ch, chMax);
+    
+    // pop ecx
+    buffer_append_u8(buffer, 0x59);
     
     // sub eax, ecx
     buffer_append_u8(buffer, 0x29);
@@ -166,20 +277,31 @@ Sub
     // neg eax
     buffer_append_u8(buffer, 0xf7);
     buffer_append_u8(buffer, 0xd8);
+    
+    return(ch);
 }
 
-void
+u8 *
 Expression
 (Buffer *buffer, u8 *ch, u8 *chMax)
 {
-    Term(buffer, ch);
-    ch = GetChar(ch, chMax);
     
-    if(ch != 0)
+    if(*ch == '-')
+    {
+        // xor eax, eax
+        buffer_append_u8(buffer, 0x31);
+        buffer_append_u8(buffer, 0xc0);
+    }
+    else
+    {
+        ch = Term(buffer, ch, chMax);
+    }
+    
+    while(ch != 0 && ((*ch == '+') || (*ch == '-')))
     {    
-        // mov ecx, eax
-        buffer_append_u8(buffer, 0x89);
-        buffer_append_u8(buffer, 0xc1);
+        // push rax
+        buffer_append_u8(buffer, 0xff);
+        buffer_append_u8(buffer, 0xf0);
         
         u8 c = *ch;
         ch = GetChar(ch, chMax);
@@ -188,12 +310,12 @@ Expression
         {
             case '+':
             {
-                Add(buffer, ch);
+                ch = Add(buffer, ch, chMax);
             }break;
             
             case '-':
             {
-                Sub(buffer, ch);
+                ch = Sub(buffer, ch, chMax);
             }break;
             
             default:
@@ -202,6 +324,8 @@ Expression
             };
         }
     }
+    
+    return(ch);
 }
 
 int __stdcall
@@ -242,6 +366,62 @@ WinMainCRTStartup
         buffer_append_u8(&buffer_functions, 0xc3); //ret
         s32 result = program();
         Assert(result == -2);
+    }
+    
+    {
+        String src = create_string(&buffer_strings, "7+8-9");
+        fn_void_to_s32 program = (fn_void_to_s32)buffer_functions.end;
+        Expression(&buffer_functions, src.chars, (src.chars + src.len));
+        buffer_append_u8(&buffer_functions, 0xc3); //ret
+        s32 result = program();
+        Assert(result == 6);
+    }
+    
+    {
+        String src = create_string(&buffer_strings, "2+3*4");
+        fn_void_to_s32 program = (fn_void_to_s32)buffer_functions.end;
+        Expression(&buffer_functions, src.chars, (src.chars + src.len));
+        buffer_append_u8(&buffer_functions, 0xc3); //ret
+        s32 result = program();
+        Assert(result == 14);
+    }
+    
+    {
+        String src = create_string(&buffer_strings, "9/3");
+        fn_void_to_s32 program = (fn_void_to_s32)buffer_functions.end;
+        Expression(&buffer_functions, src.chars, (src.chars + src.len));
+        buffer_append_u8(&buffer_functions, 0xc3); //ret
+        s32 result = program();
+        Assert(result == 3);
+    }
+    
+    {
+        String src = create_string(&buffer_strings, "1+2*3*4+5/6-7-8-9");
+        fn_void_to_s32 program = (fn_void_to_s32)buffer_functions.end;
+        Expression(&buffer_functions, src.chars, (src.chars + src.len));
+        buffer_append_u8(&buffer_functions, 0xc3); //ret
+        s32 expected = 1+2*3*4+5/6-7-8-9;
+        s32 result = program();
+        Assert(result == expected);
+    }
+    
+    {
+        String src = create_string(&buffer_strings, "1+2*(3+4)-5*(6-7*(8*9))");
+        fn_void_to_s32 program = (fn_void_to_s32)buffer_functions.end;
+        Expression(&buffer_functions, src.chars, (src.chars + src.len));
+        buffer_append_u8(&buffer_functions, 0xc3); //ret
+        s32 expected = 1+2*(3+4)-5*(6-7*(8*9));
+        s32 result = program();
+        Assert(result == expected);
+    }
+    
+    {
+        String src = create_string(&buffer_strings, "-8");
+        fn_void_to_s32 program = (fn_void_to_s32)buffer_functions.end;
+        Expression(&buffer_functions, src.chars, (src.chars + src.len));
+        buffer_append_u8(&buffer_functions, 0xc3); //ret
+        s32 result = program();
+        Assert(result == -8);
     }
     
     return(0);
