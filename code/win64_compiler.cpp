096f9f4495e9e4ba4bc92f03d116365dbc04a466
Leta <31357777+Lita6@users.noreply.github.com>
2022-10-05 18:53:51 -0500

    Lexer splits up a line on whitespace boundaries
    The line splitter can now handle if there are leading end of line characters in a file.

--- code/win64_compiler.cpp
+++ code/win64_compiler.cpp
@@ -12,6 +12,7 @@ enum Token_Type
     Token_Type_Semicolon,
     Token_Type_S32_Keyword,
     Token_Type_Main_Keyword,
+    Token_Type_Return,
     Token_Type_Immediate,
 };
 
@@ -21,6 +22,55 @@ struct Token
     String string;
     s32 value;
 };
+
+struct Token_List
+{
+    Token *start;
+    u32 count;
+};
+
+Token_List
+lex
+(Buffer *buffer, String line)
+{
+    Token_List result = {};
+    result.start = (Token *)buffer->end;
+    Token *current_token = 0;
+    for(u32 i = 0; i < line.len; i++)
+    {
+        if((line.chars[i] == ' ') || (line.chars[i] == '\t'))
+        {
+            while((line.chars[i] == ' ') || (line.chars[i] == '\t'))
+            {
+                i++;
+            }
+            
+            if(i >= line.len)
+            {
+                break;
+            }
+            
+            current_token = (Token *)buffer_allocate(buffer, sizeof(Token));
+            result.count++;
+            current_token->string.chars = &line.chars[i];
+        }
+        
+        if(current_token == 0)
+        {
+            current_token = (Token *)buffer_allocate(buffer, sizeof(Token));
+            result.count++;
+        }
+        
+        if(current_token->string.chars == 0)
+        {
+            current_token->string.chars = &line.chars[i];
+        }
+        
+        current_token->string.len++;
+    }
+    
+    return(result);
+}
 
 int __stdcall
 WinMainCRTStartup
@@ -39,8 +89,7 @@ WinMainCRTStartup
         String src = create_string(&buffer_strings, "s32 main() {\r\nreturn 2;\r\n}");
         
         String line[64] = {};
-        u32 current_line = 0;
-        line[current_line].chars = src.chars;
+        u32 line_count = 0;
         for(u32 i = 0; i < src.len; i++)
         {
             if((src.chars[i] == '\r') || (src.chars[i] == '\n'))
@@ -55,26 +104,25 @@ WinMainCRTStartup
                     break;
                 }
                 
-                current_line++;
-                line[current_line].chars = &src.chars[i];
+                if(line[line_count].chars != 0)
+                {
+                    line_count++;
+                }
+                line[line_count].chars = &src.chars[i];
             }
             
-            line[current_line].len++;
-        }
-        
-#if 0        
-        String token  = {};
-        token.chars = line.chars;
-        for(u32 i = 0; i < line.len; i++)
-        {
-            if(line.chars[i] == ' ')
+            if(line[line_count].chars == 0)
             {
-                break;
+                line[line_count].chars = &src.chars[i];
             }
-            
-            token.len++;
+            line[line_count].len++;
         }
-#endif
+        line_count++;
+        
+        for(u32 i = 0; i < line_count; i++)
+        {
+            Token_List tokens = lex(&buffer_tokens, line[i]);
+        }
         
         clear_buffer(&buffer_strings);
     }
